7a14c69f8c4eb8477f47b51b557485b274f82e7f
Darshan-B23 <Darshan.b2325@gmail.com>
2026-05-16 09:53:30 +0530

    New zsh file

---
+++ designs/D1/terminal/base.zsh
@@ -0,0 +1,14 @@
+# 1. Enable Tab Completion (Without this, pressing Tab does nothing)
+autoload -Uz compinit && compinit
+
+# 2. Command History (So the up-arrow remembers your past commands)
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt appendhistory
+
+# 3. Default Vanilla Prompt (username@machine ~/current_folder %)
+PROMPT="%n@%m %~ %# "
+
+# 4. Display system info on startup
+fastfetch
